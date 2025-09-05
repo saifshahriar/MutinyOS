422c25c5fc5a16b4b6f7d235dce087935bd2aca3
Saif Shahriar <sh.saif@proton.me>
2025-09-05 05:35:17 -0700

    add calc programme for shell

--- shell.c
+++ shell.c
@@ -4,44 +4,58 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#include "lib/compatibility.h"
+
+#include "calc.c"
+
 #define MAX_ARGS 20
 #define MAX_CMD  100
 
 int main() {
-    char line[MAX_CMD];
+	char line[MAX_CMD];
 
-    while (1) {
-        printf("name@desktop:$> ");
-        if (!fgets(line, sizeof(line), stdin))
-            break;
+	while (1) {
+		printf("name@desktop:$> ");
+		if (!fgets(line, sizeof(line), stdin))
+			break;
 
-        line[strcspn(line, "\n")] = '\0';
+		line[strcspn(line, "\n")] = '\0';
 
-        if (!strcmp(line, "exit")) {
-            printf("name@desktop:$ [EXIT]> Exiting...\n");
-            break;
-        }
+		if (!strcmp(line, "exit")) {
+			printf("name@desktop:$ [EXIT]> Exiting...\n");
+			break;
+		}
 
-        if (!strlen(line))
-            continue;
+		if (!strcmp(line, "clear") || !strcmp(line, "clr") || !strcmp(line, "cls")) {
+			clrscr();
+			continue;
+		}
 
-        // tokenize
-        char *argv[MAX_ARGS];
-        int argc = 0;  
-        char *token = strtok(line, " ");
-        while (token && argc < MAX_ARGS - 1) {
-            argv[argc++] = token;
-            token        = strtok(NULL, " ");
-        }
-        argv[argc] = NULL;
+		if (!strcmp(line, "calc")) {
+			calc();
+			continue;
+		}
 
-        pid_t pid = fork();
-        if (!pid) {
-            execvp(argv[0], argv);
-            perror("exec failed");
-            exit(1);
-        } else
-            wait(NULL);
-    }
-    return 0;
+		if (!strlen(line))
+			continue;
+
+		// tokenize
+		char *argv[MAX_ARGS];
+		int argc = 0;  
+		char *token = strtok(line, " ");
+		while (token && argc < MAX_ARGS - 1) {
+			argv[argc++] = token;
+			token        = strtok(NULL, " ");
+		}
+		argv[argc] = NULL;
+
+		pid_t pid = fork();
+		if (!pid) {
+			execvp(argv[0], argv);
+			perror("exec failed");
+			exit(1);
+		} else
+			wait(NULL);
+	}
+	return 0;
 }
