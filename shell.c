2293f326588e924bada1b10e346d1d9432ffba5a
Saif Shahriar <sh.saif@proton.me>
2025-09-05 04:22:29 -0700

    add first code for mutiny-shell

---
+++ shell.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 20
+#define MAX_CMD  100
+
+int main() {
+    char line[MAX_CMD];
+
+    while (1) {
+        printf("name@desktop:$> ");
+        if (!fgets(line, sizeof(line), stdin))
+            break;
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if (!strcmp(line, "exit")) {
+            printf("name@desktop:$ [EXIT]> Exiting...\n");
+            break;
+        }
+
+        if (!strlen(line))
+            continue;
+
+        // tokenize
+        char *argv[MAX_ARGS];
+        int argc = 0;  
+        char *token = strtok(line, " ");
+        while (token && argc < MAX_ARGS - 1) {
+            argv[argc++] = token;
+            token        = strtok(NULL, " ");
+        }
+        argv[argc] = NULL;
+
+        pid_t pid = fork();
+        if (!pid) {
+            execvp(argv[0], argv);
+            perror("exec failed");
+            exit(1);
+        } else
+            wait(NULL);
+    }
+    return 0;
+}
