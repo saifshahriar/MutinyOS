7b056b5d84f1336c142d92bdcf6ea2a660e288df
Saif Shahriar <sh.saif@proton.me>
2025-12-14 18:58:29 +0600

    bump version to 0.2.1

--- help.c
+++ help.c
@@ -14,7 +14,7 @@ void print_help() {
 	// clang-format on
 
 	printf("Welcome to MutinyOS %s!\n", VERSION);
-	puts("------------------------");
+	puts("--------------------------");
 	puts("To list all the available commands, type `cmd` or `cmds`.");
 	puts("To list all the available programs, type `prog` or `progs`.");
 	printf("Total program(s): %d, Total command(s): %d.\n\n\n", cnt_prog(),
